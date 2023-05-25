9385611032da440e1fc8abe74a770857be596f94
d4lt <daniel_sena07@outlook.com>
2023-05-25 18:05:47 -0300

    made the uuids of the seed not random

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,36 +4,36 @@ import { randomUUID } from "crypto";
 const prisma = new PrismaClient();
 
 const user1 = {
-    id: randomUUID(),
+    id: '7530c290-5212-4b6c-823b-77bed4d5afc1', 
     name: 'Alice',
 }
 
 const user2 = {
-    id: randomUUID(),
+    id: 'e418abb8-6932-424f-8188-5b01ff22f381',
     name: 'Bob',
 }
 
 const user3 = {
-    id: randomUUID(),
+    id: 'f32c9f48-ad97-4b8e-b44a-03a21b9a6f3d',
     name: 'Joe',
 }
 
 const book1 = { 
-    id: randomUUID(),
+    id: '405b820c-86fd-4f66-b8f7-76f8125b0710',
     title: 'The Art of War',
     author: 'Sun Tzu',
     isbn: '9781599869773'
 }
 
 const book2 = { 
-    id: randomUUID(),
+    id: 'c4eaaffb-a755-4f45-8f42-d4e57c69cfe1',
     title: "1984",
     author: 'George Orwell',
     isbn: '9791280035356'
 }
 
 const book3 = { 
-    id: randomUUID(),
+    id: 'a5e081b1-d1c7-4ce3-8f26-70baa3beb493',
     title: "Hamlet",
     author: 'Shakespeare',
     isbn: '9780743477123'
@@ -41,32 +41,6 @@ const book3 = {
 }
 
 
-async function populateUser() {
-
-
-}
-
-async function populateBook() {
-
-    prisma.book.create({
-        data: {
-            ...book1
-        }
-    })
-
-    prisma.book.create({
-        data: {
-            ...book2
-        }
-    })
-
-    prisma.book.create({
-        data: {
-            ...book3
-        }
-    })
-
-}
 
 async function run(){
 
