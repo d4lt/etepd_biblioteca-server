64cccbf47c9d8b12618af7ca442055b028e851a3
d4lt <daniel_sena07@outlook.com>
2023-05-25 18:04:47 -0300

    added the routes: listUsers and findUsersById in routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,19 @@
 import { Router } from "express";
 import {BookController} from "./controllers/bookController";
-import { BookPrismaRepository } from "./repositories/prismaRepository/bookPrisma";
+import { UserController } from "./controllers/userController";
+import { PrismaRepository } from "./repositories/prismaRepository/prismaRepository";
 
-const repository = new BookPrismaRepository();
+const repository = new PrismaRepository()
 
-const bookController = new BookController(repository);
+const bookController = new BookController(repository.book);
+const userController = new UserController(repository.user);
 
 const router = Router();
 
 router.get("/books", async (req, res) => await bookController.listBooks(req, res) ) 
-
 router.get("/books/:id", async (req, res) => await bookController.findBookById(req, res) )
 
-export { router, bookController }
+router.get("/users", async (req, res) => await userController.listUsers(req, res))
+router.get("/users/:id", async (req, res) => await userController.findUserById(req, res))
+
+export { router }
